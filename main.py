f6a5917eee90c4448d5906be5fe515dd926e521c
Edward Gomez <edward@gomez.me.uk>
2016-10-02 16:08:28 +0100

    Removing flask stuff

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
-from flask import Flask
-from flask import render_template
-import glob
-import os
-import time
-import allsky
+#/usr/bin/python
+import urllib2, urllib
+import time, os
+from datetime import datetime
+import RPi.GPIO as GPIO
+import picamera
+import local_settings as l
 
-app = Flask(__name__)
 
-MAX_FILES = 150
-IMAGE_DIR = '/home/pi/images/'
-BASE_URL = '/images/'
-CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
-SNAPSHOT_DIR = os.path.join(CURRENT_PATH,IMAGE_DIR)
+SNAPSHOT_DIR = os.path.join(CURRENT_PATH,'/static/images')
 
-@app.route('/')
-def hello(name=None):
-    files = [(BASE_URL+os.path.basename(x), time.ctime(os.path.getctime(x))) for x in glob.glob(SNAPSHOT_DIR + '*.jpg')]
-    lastimage =  files[-1]
-    return render_template('hello.html', filelist=files,lastimage=lastimage)
+def __main__():
+    url = 'https://api.pushover.net/1/messages.json'
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.add_event_detect(23, GPIO.RISING)
+    GPIO.add_event_callback(23, action_callback)
 
-@app.route('/snap/',methods=['GET', 'POST'])
-def snap(name=None):
-    if request.method == 'GET':
-        files = [x  for x in glob.glob(IMAGE_DIR + '*.jpg')]
-        filename =  files[-1]
-    if request.method == 'POST':
-        filename = allsky.snap()
-    return render_template('say_cheese.html', lastimage=filename, url=BASE_URL)
+def action_callback(channel):
+    print("Button pressed")
+    filename = snap()
+    img_url = "http://www.zemogle.uk/doorbell/%s" % (filename)
+    l.data['url'] = img_url
+    dataenc = urllib.urlencode(l.data)
+    content = urllib2.urlopen(url=url, data=dataenc).read()
+    print("Button Released")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+def snap():
+    filename = "image-%s.jpg" % datetime.strftime(datetime.now(),"%Y-%m-%dT%H:%M:%S")
+    camera = picamera.PiCamera()
+    camera.led = False
+    camera.resolution = (800,600)
+    #camera.shutter_speed = 20000
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture(SNAPSHOT_DIR +filename)
+    return filename
+
+def myip():
+    url = 'http://ipecho.net/plain'
+    f = urllib2.urlopen(url)
+    ip = f.read()
+    return ip
+
+__main__()
