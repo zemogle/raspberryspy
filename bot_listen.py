0897c9004a821b3c26b75553faf587b91c7c716d
Edward Gomez <edward@gomez.me.uk>
2017-01-12 13:28:41 +0000

    Remove the print lines

--- bot_listen.py
+++ bot_listen.py
@@ -7,16 +7,13 @@ import local_settings as l
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
-
     if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
+        bot.sendMessage(chat_id, 'Is there anyone there?')
         m.snap(bot)
 
 if __name__ == "__main__":
     bot = telepot.Bot(l.telegram['token'])
     bot.message_loop(handle)
-    print ('Listening ...')
 
     # Keep the program running.
     while 1:
